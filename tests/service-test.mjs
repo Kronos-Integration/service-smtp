04c7183bf722910bf4d2fc3515073f9dd7fbcf68
Markus Felten <Markus.Felten@gmx.de>
2020-11-29 02:16:52 +0100

    test: skeleton

---
+++ tests/service-test.mjs
@@ -0,0 +1,20 @@
+import test from "ava";
+import { StandaloneServiceProvider } from "@kronos-integration/service";
+import { ServiceSMTP } from "@kronos-integration/service-smtp";
+
+
+const config = {
+  type: ServiceSMTP,
+  host: 'localhost',
+  port: 1025,
+};
+
+test("service-smtp send", async t => {
+  const sp = new StandaloneServiceProvider();
+  const smtp = await sp.declareService(config);
+
+  t.deepEqual(
+    await smtp.send({ username: "user1", password: "test" }),
+    {  }
+  );
+});
